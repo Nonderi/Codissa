415f664a84350d96a32117797d776db50cdd2ebd
Elllanna <ellaapokela@gmail.com>
2022-11-30 16:19:31 +0200

    Kortit

--- codissaCats.js
+++ codissaCats.js
@@ -11,15 +11,60 @@ function Get() {
     .then((data) => {
       console.dir(data);
       let cat_data = document.getElementById("cat-tbody");
-      let cat_rows =
-        "<tr><th>id</th><th>name</th><th>color</th><th>food</th></tr>";
+      let cat_rows = "";
+
+        // "<tr><th>name</th><th>color</th><th>food</th></tr>";
       for (let cat of data) {
-        cat_rows += `<tr><td>${cat.id}</td><td>${cat.name}</td><td>${cat.color}</td><td>${cat.food}</td></tr>`;
+        cat_rows += 
+        `<form class="post">
+        <td><div id="cat-tbody" class="card" style="width: 30rem; height: 60rem;">
+        <div class="card-body"><dt>Name</dt><dd>${cat.name}</dd>
+        <dt>Age</dt><dd>${cat.age}</dd>
+        <dt>Character</dt><dd>${cat.character}</dd>
+        <dt>Color</dt><dd>${cat.color}</dd>
+        <dt>Family friendly</dt><dd>${cat.likeChildren}</dd>
+        <dt>Likes other animals</dt><dd>${cat.likeAnimal}</dd>
+        <dt>Needs companion</dt><dd>${cat.needCompanion}</dd>
+        <dt>Demanding</dt><dd>${cat.demanding}</dd>
+        <dt>Food</dt><dd>${cat.food}</dd>
+        <dt>Status</dt><dd>${cat.status}</dd>
+        <dt>Details</dt><dd>${cat.details}</dd></div>
+
+        <div class="form-group"><button class="btn btn-primary btn-block" type=button
+        onClick="parent.location='codissaFosterForm.html'">Pick me</button>
+        </div>
+        </div>
+        </td>
+        </form>
+        `;
+        // `<tr><td id ="name">${cat.name}</td><td>${cat.color}</td><td>${cat.food}</td></tr>`;
       }
       cat_data.innerHTML = cat_rows;
+
     })
     .catch((err) => console.dir(err));
+  }
+
+const showResponse = (data) => {
+
+  document.getElementById("result").style.display = "block"
+
+  document.getElementById("name").innerText = data.name
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
 
 function ApplyCat() {
   alert("Your application has been sent! We will contact you shortly!");
