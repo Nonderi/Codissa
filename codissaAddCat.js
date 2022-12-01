17baa0fb7a7499826cd76ab5eb3770b5d3728127
ninalaine <ninajlaine@hotmail.com>
2022-12-01 16:52:30 +0200

    post

--- codissaAddCat.js
+++ codissaAddCat.js
@@ -1,3 +1,13 @@
-function AddCat() {
-    alert("We will contact you shortly!");
-  }
+// function AddCat() {
+//     alert("We will contact you shortly!");
+//   }
+
+  let newbeer = { Name: "Kisu", Character: "Snuggly", Color: "Black", LikeChildren: true, LikeAnimal: false, Food:"likes everything", Status: "looking for family", Details: "-", Picture: "" }
+        const AddCat = () => {
+            fetch("https://codissacats.azurewebsites.net/api/cat", {
+                method: 'POST',
+                body: JSON.stringify(newbeer),
+                headers: { "accept": "application/json", "Content-Type": "application/json" }
+            }).then(res => res.json())
+                .then(data => console.dir(data))
+        }
