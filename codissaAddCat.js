38e892a322660b6d083b9838d30beb283b1b82b9
ninalaine <ninajlaine@hotmail.com>
2022-12-01 17:38:57 +0200

    ei toimi

--- codissaAddCat.js
+++ codissaAddCat.js
@@ -1,13 +1,57 @@
 // function AddCat() {
 //     alert("We will contact you shortly!");
 //   }
+//Get();
+function AddCat(){
+  var Name = document.getElementById("name").value;
+  var Age = document.getElementById("age").value;
+  var Character = document.getElementById("character").value;
+  var Color = document.getElementById("catcolor").value;
+  var LikeChildren = document.getElementById("likechildren").value;
+  var LikeAnimal = document.getElementById("likeanimal").value;
+  var Food = document.getElementById("food").value;
+  var Status = document.getElementById("status").value;
+  var Details = document.getElementById("details").value;
 
-  let newbeer = { Name: "Kisu", Character: "Snuggly", Color: "Black", LikeChildren: true, LikeAnimal: false, Food:"likes everything", Status: "looking for family", Details: "-", Picture: "" }
-        const AddCat = () => {
-            fetch("https://codissacats.azurewebsites.net/api/cat", {
-                method: 'POST',
-                body: JSON.stringify(newbeer),
-                headers: { "accept": "application/json", "Content-Type": "application/json" }
-            }).then(res => res.json())
-                .then(data => console.dir(data))
-        }
+  var data = JSON.stringify({
+    "name": Name,
+    "age": Age,
+    "character": Character,
+    "catcolor": Color,
+    "likechildren": LikeChildren,
+    "likeanimal": LikeAnimal,
+    "food": Food,
+    "status": Status,
+    "details": Details
+  });
+
+  fetch("https://codissacats.azurewebsites.net/api/cat",
+  {method:"POST", body:data, headers:{"Content-Type":"application/json"}})
+  .then(res=>res.json())
+  .then(data => console.dir(data))
+
+
+      // if(res.ok){
+      //     Get()
+      // }
+      // else{
+      //     console.dir(res)
+      // }
+  
+  .catch(error=>console.dir(error))
+ }
+
+
+
+
+  // let newcat = {  Status: "looking for family", Details: "-", Picture: "" }
+  //       const AddCat = () => {
+  //           fetch("https://codissacats.azurewebsites.net/api/cat", {
+  //               method: 'POST',
+  //               body: JSON.stringify(newcat),
+  //               headers: { "accept": "application/json", "Content-Type": "application/json" }
+  //           }).then(res => res.json())
+  //               .then(data => console.dir(data))
+  //       }
+
+  
