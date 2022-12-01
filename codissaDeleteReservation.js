a975917bde4975446f982975e91126960624989e
Nonderi <41451420+Nonderi@users.noreply.github.com>
2022-12-01 16:35:46 +0200

    failed tremendously

--- codissaDeleteReservation.js
+++ codissaDeleteReservation.js
@@ -21,8 +21,34 @@ function ReservationCancel(id) {
         body: JSON.stringify(cat),
       };
       fetch(url, putMethod)
-        .then((response) => response.json())
+        .then((response) => response.text())
         .then((data) => console.log(data)) // Manipulate the data retrieved back, if we want to do something with it
         .catch((err) => console.log(err)); // Do something with the error
     });
 }
+
+// function DeleteFoster(catid) {
+//   //let catid = urlParams.get("catid");
+//   //let newid = document.getElementById("id").value;
+//   console.log(catid);
+
+//   let url = "https://codissacats.azurewebsites.net/api/foster" + catid;
+//   fetch(url)
+//     .then((res) => res.json())
+//     .then((foster) => {
+//       foster.catid;
+//       console.log(foster.id);
+//       const deleteMethod = {
+//         method: "DELETE", // Method itself
+//         headers: {
+//           "Content-type": "application/json; charset=UTF-8", // Indicates the content
+//         },
+//         //body: JSON.stringify(foster),
+//       };
+
+//       fetch(url, deleteMethod)
+//         .then((response) => response.text())
+//         .then((result) => console.log(result))
+//         .catch((error) => console.log("error", error));
+//     });
+// }
