072d45f444dbae06bc1af02bb1b3540dcc499faa
ninalaine <ninajlaine@hotmail.com>
2022-12-01 20:10:30 +0200

    ei vieläkään toimi

--- codissaAddCat.js
+++ codissaAddCat.js
@@ -2,8 +2,9 @@
 //     alert("We will contact you shortly!");
 //   }
 //Get();
+const apiURL='https://codissacats.azurewebsites.net/api/cat'
 const AddCat = () => {
-  let Name = document.getElementById("Name").value;
+  let Name = document.getElementById("name").value;
   let Age = document.getElementById("age").value;
   let Character = document.getElementById("character").value;
   let Color = document.getElementById("catcolor").value;
@@ -13,31 +14,72 @@ const AddCat = () => {
   let Status = document.getElementById("status").value;
   let Details = document.getElementById("details").value;
 
-  var data = JSON.stringify({
-    "Name": Name,
-    "age": Age,
-    "character": Character,
-    "catcolor": Color,
-    "likechildren": LikeChildren,
-    "likeanimal": LikeAnimal,
-    "food": Food,
-    "status": Status,
-    "details": Details
-  });
+  let newcat={Name:Name, Age:Age, Character:Character,Color:Color,LikeChildren:LikeChildren, LikeAnimal:LikeAnimal, Food:Food, Status:Status, Details:Details}
 
-  fetch("https://codissacats.azurewebsites.net/api/cat", {
-  method: 'POST',
-  body: JSON.stringify(uusiHenkilö),
-  headers: { "accept": "application/json", "Content-Type": "application/json" }
-})
-  .then(res=>res.json())
-        .then(data=>{
-            console.dir(data)
-            showResponse(data)
-        })
-        .catch(error=>console.dir(error))
+  var myHeaders = new Headers();
+  myHeaders.append("Accept", "application/json");
+  myHeaders.append("Content-Type", "application/json");
 
-      }
+  var requestOptions = {
+    method:'POST',
+    headers: myHeaders,
+    body: JSON.stringify(newcat)
+  };
+  fetch(apiURL, requestOptions)
+    .then(res=>res.json())
+    .then(data=>{
+      console.dir(data)
+      showResponse(data)
+      console.log("toimii")
+    })
+    .catch(error=>console.dir(error))
+  }
+
+  const showResponse = (data) => {
+
+      //document.getElementById("result").style.display = "block"
+
+      document.getElementById("id").innerText = data.Id
+      document.getElementById("name").innerText = data.Name
+      document.getElementById("age").innerText = data.Age
+      document.getElementById("character").innerText = data.Character
+      document.getElementById("color").innerText = data.Color
+      document.getElementById("likechildren").innerText = data.LikeChildren
+      document.getElementById("likeanimal").innerText = data.LikeAnimal
+      document.getElementById("food").innerText = data.Food
+      document.getElementById("status").innerText = data.Status
+      document.getElementById("details").innerText = data.Details
+
+
+
+
+  }
+
+//   var data = JSON.stringify({
+//     "Name": Name,
+//     "age": Age,
+//     "character": Character,
+//     "catcolor": Color,
+//     "likechildren": LikeChildren,
+//     "likeanimal": LikeAnimal,
+//     "food": Food,
+//     "status": Status,
+//     "details": Details
+//   });
+
+//   fetch("https://codissacats.azurewebsites.net/api/cat", {
+//   method: 'POST',
+//   body: JSON.stringify(AddCat),
+//   headers: { "accept": "application/json", "Content-Type": "application/json" }
+// })
+//   .then(res=>res.json())
+//         .then(data=>{
+//             console.dir(data)
+//             showResponse(data)
+//         })
+//         .catch(error=>console.dir(error))
+
+//       }
 
 //   fetch("https://codissacats.azurewebsites.net/api/cat",
 //   {method:"POST", body:data, headers:{"Content-Type":"application/json"}})
