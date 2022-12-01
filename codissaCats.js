ef7627a927d3fd2ed4c977e8d04792cd3011fdf7
Elllanna <ellaapokela@gmail.com>
2022-12-01 16:38:16 +0200

    Keskelle kissat

--- codissaCats.js
+++ codissaCats.js
@@ -17,46 +17,54 @@ function Get() {
       for (let cat of data) { 
         cat_rows += 
         `
-        <td><div id="cat-tbody" class="card" style="width: 30rem; height: 60rem;">
-          
-        <div class="card-body"><dd>
-              <img src="${cat.picture}"
-            width=420px></a></dd>
-          </div>
-        </td>
-        
         <form class="post">
-        <td><div id="cat-tbody" class="card" style="width: 30rem; height: 60rem;">
-        <div class="card-body"><dt>Name</dt><dd>${cat.name}</dd>
-        <dt>Age</dt><dd>${cat.age}</dd>
-        <dt>Character</dt><dd>${cat.character}</dd>
-        <dt>Color</dt><dd>${cat.color}</dd>
-        <dt>Family friendly</dt><dd>${cat.likeChildren}</dd>
-        <dt>Likes other animals</dt><dd>${cat.likeAnimal}</dd>
-        <dt>Needs companion</dt><dd>${cat.needCompanion}</dd>
-        <dt>Demanding</dt><dd>${cat.demanding}</dd>
-        <dt>Food</dt><dd>${cat.food}</dd>
-        <dt>Status</dt><dd>${cat.status}</dd>
-        <dt>Details</dt><dd>${cat.details}</dd></div>
+        <div id="cat-tbody" class="card" style="width: 30rem; height: 60rem;">
+            <td>
+                <div class="card-body">
+                    <dd><img src="${cat.picture}" width=420px></a></dd>
+                </div>
+            </td>
+        
+            <td>
+                <div id="cat-tbody" class="card" style="width: 30rem; height: 60rem;">
+                    <div class="card-body">
+                    <dt>Name</dt>
+                    <dd>${cat.name}</dd>
+                    <dt>Age</dt>
+                    <dd>${cat.age}</dd>
+                    <dt>Character</dt>
+                    <dd>${cat.character}</dd>
+                    <dt>Color</dt>
+                    <dd>${cat.color}</dd>
+                    <dt>Family friendly</dt>
+                    <dd>${cat.likeChildren}</dd>
+                    <dt>Likes other animals</dt>
+                    <dd>${cat.likeAnimal}</dd>
+                    <dt>Needs companion</dt>
+                    <dd>${cat.needCompanion}</dd>
+                    <dt>Demanding</dt>
+                    <dd>${cat.demanding}</dd>
+                    <dt>Food</dt>
+                    <dd>${cat.food}</dd>
+                    <dt>Status</dt>
+                    <dd>${cat.status}</dd>
+                    <dt>Details</dt>
+                    <dd>${cat.details}</dd>
+                </div>
 
-        <div class="form-group"><button class="btn btn-primary btn-block" type=button
-        onClick="parent.location='codissaFosterForm.html?catid=${cat.id}'">Pick me</button>
-        </div> <br>
-        <div class="form-group"><button class="btn btn-primary btn-block" type=button
-        onClick="parent.location='codissaDeleteReservation.html?catid=${cat.id}'">Cancel reservation</button>
-        </div>
-        </div>
-        </td>
+                    <div class="form-group"><button class="btn btn-primary btn-block" type=button
+                    onClick="parent.location='codissaFosterForm.html?catid=${cat.id}'">Pick me</button>
+                    </div> <br>
+                    <div class="form-group"><button class="btn btn-primary btn-block" type=button
+                    onClick="parent.location='codissaDeleteReservation.html?catid=${cat.id}'">Cancel reservation</button>
+                    </div>
+                </div>
+            </td>
         </form>
+        <br><br>
+        <br><br>
         `;
         // `<tr><td id ="name">${cat.name}</td><td>${cat.color}</td><td>${cat.food}</td></tr>`;
-
-
-
-        
-
-
-
         //<dt>Picture</dt><dd><img src="${cat.picture}"></dd>
       }
       cat_data.innerHTML = cat_rows;
