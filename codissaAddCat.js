47ad686ed0b3a0770aa3e5494f637159e74bad93
Nonderi <41451420+Nonderi@users.noreply.github.com>
2022-12-01 21:07:51 +0200

    valmis

--- codissaAddCat.js
+++ codissaAddCat.js
@@ -2,58 +2,69 @@
 //     alert("We will contact you shortly!");
 //   }
 //Get();
-const apiURL='https://codissacats.azurewebsites.net/api/cat'
+const apiURL = "https://codissacats.azurewebsites.net/api/cat";
 const AddCat = () => {
   let Name = document.getElementById("name").value;
   let Age = document.getElementById("age").value;
   let Character = document.getElementById("character").value;
   let Color = document.getElementById("catcolor").value;
-  let LikeChildren = document.getElementById("likechildren").value;
-  let LikeAnimal = document.getElementById("likeanimal").value;
+  let LikeChildren = document.getElementById("likechildren").checked;
+  let LikeAnimal = document.getElementById("likeanimal").checked;
   let Food = document.getElementById("food").value;
   let Status = document.getElementById("status").value;
   let Details = document.getElementById("details").value;
+  let Demanding = document.getElementById("demanding").checked;
 
-  let newcat={Name:Name, Age:Age, Character:Character,Color:Color,LikeChildren:LikeChildren, LikeAnimal:LikeAnimal, Food:Food, Status:Status, Details:Details}
+  let Picture = document.getElementById("picture").value;
+
+  let newcat = {
+    Name: Name,
+    Age: Age,
+    Character: Character,
+    Color: Color,
+    LikeChildren: LikeChildren,
+    LikeAnimal: LikeAnimal,
+    Food: Food,
+    Status: Status,
+    Details: Details,
+    Demanding: Demanding,
+    Picture: Picture,
+  };
 
   var myHeaders = new Headers();
   myHeaders.append("Accept", "application/json");
   myHeaders.append("Content-Type", "application/json");
 
   var requestOptions = {
-    method:'POST',
+    method: "POST",
     headers: myHeaders,
-    body: JSON.stringify(newcat)
+    body: JSON.stringify(newcat),
   };
   fetch(apiURL, requestOptions)
-    .then(res=>res.json())
-    .then(data=>{
-      console.dir(data)
-      showResponse(data)
-      console.log("toimii")
+    .then((res) => res.json())
+    .then((data) => {
+      console.dir(data);
+      showResponse(data);
+      console.log("toimii");
     })
-    .catch(error=>console.dir(error))
-  }
+    .catch((error) => console.dir(error));
+};
 
-  const showResponse = (data) => {
+const showResponse = (data) => {
+  //document.getElementById("result").style.display = "block"
 
-      //document.getElementById("result").style.display = "block"
-
-      document.getElementById("id").innerText = data.Id
-      document.getElementById("name").innerText = data.Name
-      document.getElementById("age").innerText = data.Age
-      document.getElementById("character").innerText = data.Character
-      document.getElementById("color").innerText = data.Color
-      document.getElementById("likechildren").innerText = data.LikeChildren
-      document.getElementById("likeanimal").innerText = data.LikeAnimal
-      document.getElementById("food").innerText = data.Food
-      document.getElementById("status").innerText = data.Status
-      document.getElementById("details").innerText = data.Details
-
-
-
-
-  }
+  document.getElementById("id").innerText = data.Id;
+  document.getElementById("name").innerText = data.Name;
+  document.getElementById("age").innerText = data.Age;
+  document.getElementById("character").innerText = data.Character;
+  document.getElementById("color").innerText = data.Color;
+  document.getElementById("likechildren").innerText = data.LikeChildren;
+  document.getElementById("likeanimal").innerText = data.LikeAnimal;
+  document.getElementById("food").innerText = data.Food;
+  document.getElementById("status").innerText = data.Status;
+  document.getElementById("details").innerText = data.Details;
+  document.getElementById("demanding").innerText = data.Demanding;
+};
 
 //   var data = JSON.stringify({
 //     "Name": Name,
@@ -86,28 +97,22 @@ const AddCat = () => {
 //   .then(res=>res.json())
 //   .then(data => console.dir(data))
 
-
 //       // if(res.ok){
 //       //     Get()
 //       // }
 //       // else{
 //       //     console.dir(res)
 //       // }
-  
+
 //   .catch(error=>console.dir(error))
 //  }
 
-
-
-
-  // let newcat = {  Status: "looking for family", Details: "-", Picture: "" }
-  //       const AddCat = () => {
-  //           fetch("https://codissacats.azurewebsites.net/api/cat", {
-  //               method: 'POST',
-  //               body: JSON.stringify(newcat),
-  //               headers: { "accept": "application/json", "Content-Type": "application/json" }
-  //           }).then(res => res.json())
-  //               .then(data => console.dir(data))
-  //       }
-
-  
+// let newcat = {  Status: "looking for family", Details: "-", Picture: "" }
+//       const AddCat = () => {
+//           fetch("https://codissacats.azurewebsites.net/api/cat", {
+//               method: 'POST',
+//               body: JSON.stringify(newcat),
+//               headers: { "accept": "application/json", "Content-Type": "application/json" }
+//           }).then(res => res.json())
+//               .then(data => console.dir(data))
+//       }
--- codissaCats.js
+++ codissaCats.js
@@ -13,20 +13,16 @@ function Get() {
       let cat_data = document.getElementById("cat-tbody");
       let cat_rows = "";
 
-        // "<tr><th>name</th><th>color</th><th>food</th></tr>";
-      for (let cat of data) { 
-        cat_rows += 
-        `
-        <form class="post">
-        <div id="cat-tbody" class="card" style="width: 30rem; height: 60rem;">
-            <td>
+      // "<tr><th>name</th><th>color</th><th>food</th></tr>";
+      for (let cat of data) {
+        cat_rows += `
+        <div class="card" style=" display: grid; grid-template-columns: 1fr 1fr">
+            
                 <div class="card-body">
                     <dd><img src="${cat.picture}" width=420px></a></dd>
                 </div>
-            </td>
-        
-            <td>
-                <div id="cat-tbody" class="card" style="width: 30rem; height: 60rem;">
+            
+                <div id="cat-tbody" class="card">
                     <div class="card-body">
                     <dt>Name</dt>
                     <dd>${cat.name}</dd>
@@ -52,51 +48,28 @@ function Get() {
                     <dd>${cat.details}</dd>
                 </div>
 
-                    <div class="form-group"><button class="btn btn-primary btn-block" type=button
+                    <div class="form-group"><button class="btn btn-primary btn-block my-button" type="button"
                     onClick="parent.location='codissaFosterForm.html?catid=${cat.id}'">Pick me</button>
                     </div> <br>
-                    <div class="form-group"><button class="btn btn-primary btn-block" type=button
+                    <div class="form-group"><button class="btn btn-primary btn-block my-button" type="button"
                     onClick="parent.location='codissaDeleteReservation.html?catid=${cat.id}'">Cancel reservation</button>
                     </div><br>
-                    <div class="form-group"><button class="btn btn-primary btn-block" type=button
+                    <div class="form-group"><button class="btn btn-primary btn-block my-button" type="button"
                     onClick="parent.location='DeleteCat.html?catid=${cat.id}'">Delete cat</button>
                     </div>
-                </div>
-            </td>
-        </form>
-        <br><br>
-        <br><br>
+          </div>
+          </div>
         `;
         // `<tr><td id ="name">${cat.name}</td><td>${cat.color}</td><td>${cat.food}</td></tr>`;
         //<dt>Picture</dt><dd><img src="${cat.picture}"></dd>
       }
       cat_data.innerHTML = cat_rows;
-
     })
     .catch((err) => console.dir(err));
-  }
-
-const showResponse = (data) => {
-
-  document.getElementById("result").style.display = "block"
-
-  document.getElementById("name").innerText = data.name
-
-
-
 }
 
+const showResponse = (data) => {
+  document.getElementById("result").style.display = "block";
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+  document.getElementById("name").innerText = data.name;
+};
